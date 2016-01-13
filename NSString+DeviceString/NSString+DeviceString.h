19a3688c4121ad7f37e09c73e1e438c8aa7b1bb7
Kito0615 <anar0615@sina.com>
2016-01-13 13:36:57 +0800

    Init

---
+++ NSString+DeviceString/NSString+DeviceString.h
@@ -0,0 +1,15 @@
+//
+//  NSString+DeviceString.h
+//  NSString+DeviceString
+//
+//  Created by AnarL on 1/13/16.
+//  Copyright © 2016 AnarL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (DeviceString)
+
++ (NSString *)deviceString;
+
+@end
